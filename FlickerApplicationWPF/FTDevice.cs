66aa192689701c3443ca07c0aca13bd57d228bbf
jjlewando <jarek.lewandowski@homersoft.com>
2013-01-26 00:32:32 +0100

    Done: Opened Device, Example of data Send
    TO DO:
    - Listener
    - Data parser to file and saving

---
+++ FlickerApplicationWPF/FTDevice.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FTD2XX_NET;
+
+namespace FlickerApplication
+{
+    class FTDevice
+    {                
+        // Create new instance of the FTDI device class
+        private static FTDI myFtdiDevice;
+        private bool opened { get; set; }
+
+        public FTDevice()
+        {
+            myFtdiDevice = new FTDI();
+            opened = false; 
+            
+        }
+
+        public bool isOpened()
+        {
+            return opened; 
+        }
+
+
+        public UInt32 GetNumberOfDevices()
+        {
+            UInt32 ftdiDeviceCount = 0;
+            FTDI.FT_STATUS ftStatus = FTDI.FT_STATUS.FT_OK;
+            
+            // Determine the number of FTDI devices connected to the machine
+            ftStatus = myFtdiDevice.GetNumberOfDevices(ref ftdiDeviceCount);
+            // Check status
+            if (ftStatus == FTDI.FT_STATUS.FT_OK)
+            {
+                Console.WriteLine("Number of FTDI devices: " + ftdiDeviceCount.ToString());
+                Console.WriteLine("");
+                return ftdiDeviceCount;
+            }
+            else
+            {
+                // Wait for a key press
+                Console.WriteLine("Failed to get number of devices (error " + ftStatus.ToString() + ")");
+                Console.ReadKey();
+                return 0;                
+            }            
+        }
+
+        public FTDI.FT_DEVICE_INFO_NODE[] GetDevices(UInt32 numberOfDevices)
+        {
+            // Allocate storage for device info list
+            FTDI.FT_DEVICE_INFO_NODE[] ftdiDeviceList = new FTDI.FT_DEVICE_INFO_NODE[numberOfDevices];
+            FTDI.FT_STATUS ftStatus = FTDI.FT_STATUS.FT_OK;
+
+
+            // Populate our device list
+            ftStatus = myFtdiDevice.GetDeviceList(ftdiDeviceList);
+
+            if (ftStatus == FTDI.FT_STATUS.FT_OK)
+            {
+                for (UInt32 i = 0; i < numberOfDevices; i++)
+                {
+                    Console.WriteLine("Device Index: " + i.ToString());
+                    Console.WriteLine("Flags: " + String.Format("{0:x}", ftdiDeviceList[i].Flags));
+                    Console.WriteLine("Type: " + ftdiDeviceList[i].Type.ToString());
+                    Console.WriteLine("ID: " + String.Format("{0:x}", ftdiDeviceList[i].ID));
+                    Console.WriteLine("Location ID: " + String.Format("{0:x}", ftdiDeviceList[i].LocId));
+                    Console.WriteLine("Serial Number: " + ftdiDeviceList[i].SerialNumber.ToString());
+                    Console.WriteLine("Description: " + ftdiDeviceList[i].Description.ToString());
+                    Console.WriteLine("");
+                    
+                }
+            }
+            return ftdiDeviceList;
+        }
+
+        public bool OpenDefaultDevice(FTDI.FT_DEVICE_INFO_NODE[] devList, UInt32 deviceIndex)
+        {
+            FTDI.FT_STATUS ftStatus = FTDI.FT_STATUS.FT_OK;            
+
+            // Open first device in our list by serial number
+            ftStatus = myFtdiDevice.OpenBySerialNumber(devList[0].SerialNumber);
+            if (ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                // Wait for a key press
+                Console.WriteLine("Failed to open device (error " + ftStatus.ToString() + ")");
+                //Console.ReadKey();
+                return false;
+            }
+
+            // Set up device data parameters
+            // Set Baud rate to 9600
+            ftStatus = myFtdiDevice.SetBaudRate(9600);
+            if (ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                // Wait for a key press
+                Console.WriteLine("Failed to set Baud rate (error " + ftStatus.ToString() + ")");
+                //Console.ReadKey();
+                return false;
+            }
+
+            // Set data characteristics - Data bits, Stop bits, Parity
+            ftStatus = myFtdiDevice.SetDataCharacteristics(FTDI.FT_DATA_BITS.FT_BITS_8, FTDI.FT_STOP_BITS.FT_STOP_BITS_1, FTDI.FT_PARITY.FT_PARITY_NONE);
+            if (ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                // Wait for a key press
+                Console.WriteLine("Failed to set data characteristics (error " + ftStatus.ToString() + ")");
+                //Console.ReadKey();
+                return false;
+            }
+
+            // Set flow control - set RTS/CTS flow control
+            ftStatus = myFtdiDevice.SetFlowControl(FTDI.FT_FLOW_CONTROL.FT_FLOW_NONE, 0x00, 0x00);
+            if (ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                // Wait for a key press
+                Console.WriteLine("Failed to set flow control (error " + ftStatus.ToString() + ")");
+                //Console.ReadKey();
+                return false;
+            }
+
+            // Set read timeout to 5 seconds, write timeout to infinite
+            ftStatus = myFtdiDevice.SetTimeouts(5000, 0);
+            if (ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                // Wait for a key press
+                Console.WriteLine("Failed to set timeouts (error " + ftStatus.ToString() + ")");
+                Console.ReadKey();
+                return false;
+            }
+            opened = true; 
+            return true; 
+        }
+
+        public bool writeData(byte[] dataBuffer)
+        {
+            FTDI.FT_STATUS ftStatus = FTDI.FT_STATUS.FT_OK;
+            
+            UInt32 numBytesWritten = 0;
+            // Note that the Write method is overloaded, so can write string or byte array data
+            ftStatus = myFtdiDevice.Write(dataBuffer, dataBuffer.Length, ref numBytesWritten);
+            if (ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                // Wait for a key press
+                Console.WriteLine("Failed to write to device (error " + ftStatus.ToString() + ")");
+                //Console.ReadKey();
+                return false;
+            }
+            return true;
+        }
+
+        public bool writeData(string dataBuffer)
+        {
+            FTDI.FT_STATUS ftStatus = FTDI.FT_STATUS.FT_OK;
+
+            UInt32 numBytesWritten = 0;
+            // Note that the Write method is overloaded, so can write string or byte array data
+            ftStatus = myFtdiDevice.Write(dataBuffer, dataBuffer.Length, ref numBytesWritten);
+            if (ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                // Wait for a key press
+                Console.WriteLine("Failed to write to device (error " + ftStatus.ToString() + ")");
+                //Console.ReadKey();
+                return false;
+            }
+            return true;
+        }
+
+
+        public bool Close()
+        {
+            myFtdiDevice.Close();
+            opened = false;
+            return true;
+        }
+
+
+
+
+
+
+    }
+}
--- FlickerApplicationWPF/MainWindow.xaml.cs
+++ FlickerApplicationWPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace FlickerApplication
 {
     /// <summary>
@@ -19,14 +20,95 @@ namespace FlickerApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static FTDevice myFlicer = new FTDevice();
+        
+
+
         public MainWindow()
         {
             InitializeComponent();
+            enableControls(true, false, false, false);        
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void enableControls(bool keyConnect, bool keyReed, bool keyErase, bool keySynchro)
         {
+            //btConnect.IsEnabled = keyConnect;
+            if (keyConnect == true) btConnect.Content = "Connect Flicker";
+            else btConnect.Content = "Disconnect Flicker";
 
+            btRead.IsEnabled = keyReed;
+            btErase.IsEnabled = keyErase;
+            btSynchro.IsEnabled = keySynchro;
+        }
+
+        private void buttonConnect(object sender, RoutedEventArgs e)
+        {
+            if (myFlicer.isOpened())
+            {
+                //Device is open
+                myFlicer.Close();
+                enableControls(true, false, false, false);
+                textStatus.Text += Environment.NewLine + "Disconnected";
+            }
+            else
+            {
+                UInt32 numberOfDevices = myFlicer.GetNumberOfDevices();
+                bool state;
+                if (numberOfDevices != 0)
+                {
+
+                    state = myFlicer.OpenDefaultDevice(myFlicer.GetDevices(numberOfDevices), 0);
+                    if (state)
+                    {
+                        textStatus.Text += Environment.NewLine + "Connected";
+                        enableControls(false, true, true, true);
+                    }
+                    else
+                    {
+                        textStatus.Text += Environment.NewLine + "Connection Error";
+                    }
+
+                }
+                else
+                {
+                    textStatus.Text += Environment.NewLine + "No devices";
+                }
+                
+            }
+            textStatus.ScrollToEnd();
+        }
+
+        private void btRead_Click(object sender, RoutedEventArgs e)
+        {
+            if (myFlicer.isOpened())
+            {
+                myFlicer.writeData("Hello");
+                textStatus.Text += Environment.NewLine + "R-Command Send";
+                textStatus.ScrollToEnd();
+            }
+        }
+
+        private void btErase_Click(object sender, RoutedEventArgs e)
+        {
+            if (myFlicer.isOpened())
+            {
+                myFlicer.writeData("Bye");
+                textStatus.Text += Environment.NewLine + "E-Command Send";
+                textStatus.ScrollToEnd();
+            }
+        }
+
+        private void btSynchro_Click(object sender, RoutedEventArgs e)
+        {
+            if (myFlicer.isOpened())
+            {
+                byte dtCode = 0xA0;
+                DateTime time = DateTime.Now;
+                Console.WriteLine(time.ToString());
+                myFlicer.writeData(time.ToString());
+                textStatus.Text += Environment.NewLine + time.ToString();
+                textStatus.ScrollToEnd();
+            }
         }
     }
 }
